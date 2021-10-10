69c816e6daf15dd8b924acf0b85ef389392093a0
Adam Guan <86337369+AdamHGuan@users.noreply.github.com>
2021-10-09 19:18:13 -0700

    Wrapping fetch requests with CSRF

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,4 +4,34 @@ const apiRouter = require("./api");
 
 router.use("/api", apiRouter);
 
+// Static routes
+
+// Serve React build files in production
+if (process.env.NODE_ENV === "production") {
+	const path = require("path");
+	router.get("/", (req, res) => {
+		res.cookie("XSRF-TOKEN", req.csrfToken());
+		return res.sendFile(
+			path.resolve(__dirname, "../../frontend", "build", "index.html")
+		);
+	});
+
+	router.use(express.static(path.resolve("../frontend/build")));
+
+	router.get(/^(?!\/?api).*/, (req, res) => {
+		res.cookie("XSRF-TOKEN", req.csrfToken());
+		return res.sendFile(
+			path.resolve(__dirname, "../../frontend", "build", "index.html")
+		);
+	});
+}
+
+// Add a XSRF-TOKEN cookie in development
+if (process.env.NODE_ENV !== "production") {
+	router.get("/api/csrf/restore", (req, res) => {
+		res.cookie("XSRF-TOKEN", req.csrfToken());
+		return res.json({});
+	});
+}
+
 module.exports = router;
