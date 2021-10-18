c93e79fee7161d34011231bbc1bd4671592cc372
Adam Guan <86337369+AdamHGuan@users.noreply.github.com>
2021-10-17 18:57:15 -0700

    Second MVP completed

--- frontend/src/components/QuestionAnswersPage/QuestionAnswersPage.js
+++ frontend/src/components/QuestionAnswersPage/QuestionAnswersPage.js
@@ -5,20 +5,19 @@ import "./QuestionAnswersPage.css";
 
 const QuestionAnswersPage = ({
 	question,
-	editAnswerId,
-	deleteAnswerId,
+	// editAnswerId,
+	// deleteAnswerId,
 	setEditAnswerId,
 	setDeleteAnswerId,
 }) => {
 	const answers = useSelector((state) => Object.values(state.answers));
 
 	const dispatch = useDispatch();
-	const user = useSelector((state) => state.session.user.id);
+	const user = useSelector((state) => state.session.user);
 	// const userAnswers = answers.filter((answer) => answer.ownerId);
-	console.log(setEditAnswerId);
 
-	const editableAnswer = user?.id === answers?.editAnswerId?.ownerId;
-	const deletableAnswer = user?.id === answers?.editAnswerId?.ownerId;
+	// const editableAnswer = user?.id === answers[editAnswerId]?.ownerId;
+	// const deletableAnswer = user?.id === answers[deleteAnswerId]?.ownerId;
 
 	useEffect(() => {
 		dispatch(getAnswers(question.id));
@@ -29,7 +28,7 @@ const QuestionAnswersPage = ({
 			{answers.map((answer) => (
 				<div key={answer.id} className="answerContainer">
 					<div>{answer.answer}</div>
-					{editableAnswer && (
+					{user.id === answer.ownerId && (
 						<div>
 							<button
 								onClick={() => setEditAnswerId(answer.id)}
@@ -39,7 +38,7 @@ const QuestionAnswersPage = ({
 							</button>
 						</div>
 					)}
-					{deletableAnswer && (
+					{user.id === answer.ownerId && (
 						<div>
 							<button
 								onClick={() => setDeleteAnswerId(answer.id)}
